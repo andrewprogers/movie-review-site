feb44eeecde49c350bdec2dd7a20e0276ef3524a
Andrew Rogers <andrewprogers@gmail.com>
2017-07-18 13:53:10 -0400

    Fixes based on hound review

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:username){ |n| "user#{n}"}
+    sequence(:username) { |n| "user#{n}" }
     password "foobar"
-    email {"#{username}@example.com"}
+    email { "#{username}@example.com" }
     first_name "Foo"
     last_name "Bar"
   end
--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -9,10 +9,11 @@ feature "User Signs In" do
   # If a user is not signed in, they should see a link to sign in on every page
   # Clicking on the sign in link will take them to a form to sign in
   # They will be required to fill in information for a valid user
-  # If they supply invalid credentials, they should be given an error message and remain on the same page
-  # If they supply valid credentials, they should be given a notice that they have successfully signed in
+  # If they supply invalid credentials, they should be given an error message and
+  # remain on the same page
+  # If they supply valid credentials, they should be given a notice that they
+  # have successfully signed in
   # Once signed in, their username should be displayed on the banner.
-
 
   scenario "User views the sign in page" do
     visit(root_path)
@@ -24,24 +25,23 @@ feature "User Signs In" do
   end
 
   scenario "User signs in with valid credentials" do
-    user = FactoryGirl.create(:user, email: "sathvik@yahoo.com", password: "password")
+    user = FactoryGirl.create(:user)
     visit(root_path)
     click_link 'Sign In'
-    fill_in("Email", with: user.email )
-    fill_in("Password", with: user.password )
+    fill_in("Email", with: user.email)
+    fill_in("Password", with: user.password)
     click_button("Log in")
     expect(page).to have_content("Signed in successfully.")
     expect(page).to_not have_content("Sign In")
     expect(page).to_not have_content("Sign Up")
-    expect(page).to have_content("#{user.username}")
-
+    expect(page).to have_content(user.username)
   end
 
   scenario "User signs in with invalid credentials" do
     visit(root_path)
     click_link 'Sign In'
-    fill_in("Email", with: "junk_username" )
-    fill_in("Password", with: "junk_password" )
+    fill_in("Email", with: "junk_username")
+    fill_in("Password", with: "junk_password")
     click_button("Log in")
     expect(page).to have_content("Invalid Email or password")
     expect(page).to_not have_content("Welcome")
