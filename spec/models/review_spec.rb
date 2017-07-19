7fa9322d3602bda5949fb948c85d009bb6363a1c
Andrew Rogers <andrewprogers@gmail.com>
2017-07-19 14:27:47 -0400

    setup app revew model and validation

---
+++ spec/models/review_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Review, type: :model do
+  it { should have_valid(:rating).when(1, 2, 3, 4, 5, 6, 7) }
+  it { should_not have_valid(:rating).when('', nil, 0, 8, 3.5, -1) }
+
+  it { should have_valid(:body).when('this is a great movie', '') }
+  end
+end
