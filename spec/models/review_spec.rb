f449a55a87fc0f5ba956e70f184ff2623f10ad40
Andrew Rogers <andrewprogers@gmail.com>
2017-07-19 16:08:29 -0400

    Fix whitespace issues

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Review, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:movie) }
-  
+
   it { should have_valid(:rating).when(1, 2, 3, 4, 5, 6, 7) }
   it { should_not have_valid(:rating).when('', nil, 0, 8, 3.5, -1) }
 
