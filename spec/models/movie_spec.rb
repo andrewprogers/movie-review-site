46c9c1bcd791dc5a065c6ba4fa62e86820bee041
Andrew Rogers <andrewprogers@gmail.com>
2017-07-19 16:06:10 -0400

    add association validations

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
+  it { should have_many(:reviews) }
+  
   it { should have_valid(:name).when('Sathvik', 'Andrew', 'George', 'Mike') }
   it { should_not have_valid(:name).when('', nil) }
   it { should have_valid(:description).when('Sathvik', 'Andrew', 'George', 'Mike') }
