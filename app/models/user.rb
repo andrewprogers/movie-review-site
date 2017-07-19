975e038424eaed008c07540d5d1c48c1d03bf8e1
Mike <mlattanz25@gmail.com>
2017-07-19 16:52:45 -0400

    fixed hound

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-has_many :movies
+  has_many :movies
 end
