9a8ca0a700a4dc959fd27d9066a2313ccd91c033
Mike <mlattanz25@gmail.com>
2017-07-19 11:31:44 -0400

    implemented and tested index, new, and show for movies

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,35 @@
+class MoviesController < ApplicationController
+before_action :authorize_user, except: [:index, :show]
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: "Movie successfully added."
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :description)
+  end
+
+  def authorize_user
+    if !user_signed_in?
+      raise ActionController::RoutingError.new("Not Found")
+    end
+  end
+end
