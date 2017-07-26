00eefc87656decedd1b6eac2be7adb7162380e8f
Andrew Rogers <andrewprogers@gmail.com>
2017-07-26 16:57:39 -0400

    Add link, poster and styling to react page

--- app/javascript/react/src/components/MovieReviewTile.js
+++ app/javascript/react/src/components/MovieReviewTile.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
 const MovieReviewTile = props => {
+  let imageSrc
+  if (props.review.movie.poster) {
+    imageSrc = props.review.movie.poster.thumb.url;
+  }
+  let movieLink = `/movies/${props.review.movie.id}`
   return(
     <div className="movie-review-tile">
-      <h4 className="movie-name">{props.review.movie.name}</h4>
-      <span className="reviewer"><b>Reviewer:</b> {props.review.user.username}</span><br />
-      <span className="rating"><b>Rating:</b> {props.review.rating}</span>
-      <p className="review-body">{props.review.body}</p>
+      <div className="row">
+        <div className="thumb-container">
+          <img src={imageSrc} />
+        </div>
+        <div className="small-10 columns">
+          <a href={movieLink}>
+            <span className="movie-name">{props.review.movie.name}</span>
+          </a>
+          <span className="rating"><b>Rating:</b> {props.review.rating}</span>
+          <span className="reviewer"><b> - Reviewed By:</b> {props.review.user.username}</span><br />
+          <hr />
+          <p className="review-body">{props.review.body}</p>
+        </div>
+      </div>
     </div>
   )
 }
